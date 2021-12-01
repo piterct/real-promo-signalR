e7c26e69aed8264095b371c11a452b2b57eeb6d6
Michael Peter <michael_piterct@hotmail.com>
2021-12-01 00:30:32 -0300

    promo hub

---
+++ RealPromo.SignalR/RealPromo.API/Hubs/PromoHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using RealPromo.API.Models;
+using System.Threading.Tasks;
+
+namespace RealPromo.API.Hubs
+{
+    public class PromoHub : Hub
+    {
+        public async Task CadastrarPromocao(Promocao promocao)
+        {
+
+        }
+    }
+}
---
+++ RealPromo.SignalR/RealPromo.API/Models/Promocao.cs
@@ -0,0 +1,10 @@
+﻿namespace RealPromo.API.Models
+{
+    public class Promocao
+    {
+        public string Empresa { get; set; }
+        public string Chamada { get; set; }
+        public string Regras { get; set; }
+        public string EnderecoURL { get; set; }
+    }
+}
