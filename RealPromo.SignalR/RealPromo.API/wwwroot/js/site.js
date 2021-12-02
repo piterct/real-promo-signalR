dc9b60775a1ad05c4f2acb7511da7c061443a3a3
Michael Peter <michael_piterct@hotmail.com>
2021-12-02 01:03:24 -0300

    Js Methods

--- RealPromo.SignalR/RealPromo.API/wwwroot/js/site.js
+++ RealPromo.SignalR/RealPromo.API/wwwroot/js/site.js
@@ -1,1 +1,14 @@
 ﻿
+var btnCadastrar = document.getElementyById("BtnCadastrar");
+
+if (btnCadastrar != null) {
+    btnCadastrar.addEventListener("click", function () {
+
+        var empresa = document.getElementyById("Empresa").value;
+        var chamada = document.getElementyById("Chamada").value;
+        var regras = document.getElementyById("Regras").value;
+        var enderecoUrl = document.getElementyById("EnderecoUrl").value;
+
+        var promocao = { Empresa: empresa, Chamada: chamada, Regras: regras, EnderecoUrl: enderecoUrl };
+    })
+}
