59a1085f3532972693da816ba9c2f40395af53bb
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-01-08 15:28:53 -0300

    JWT configurantion

--- RealPromo.SignalR/RealPromo.API/Startup.cs
+++ RealPromo.SignalR/RealPromo.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Primitives;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using RealPromo.API.Hubs;
 using System;
 using System.Collections.Generic;
@@ -52,8 +53,9 @@ namespace RealPromo.API
              {
                  OnMessageReceived = context =>
                  {
-                     if ((context.Request.Path.Value!.StartsWith("/signalrhome")
-                         || context.Request.Path.Value!.StartsWith("/looney")
+                     if ((
+                         //context.Request.Path.Value!.StartsWith("/PromoHub" || )
+                         context.Request.Path.Value!.StartsWith("/looney")
                          || context.Request.Path.Value!.StartsWith("/usersdm")
                         )
                          && context.Request.Query.TryGetValue("token", out StringValues token)
@@ -74,11 +76,45 @@ namespace RealPromo.API
 
             services.AddControllersWithViews();
             services.AddSignalR();
+
+            services.AddSwaggerGen(c =>
+            {
+                // add JWT Authentication
+                var securityScheme = new OpenApiSecurityScheme
+                {
+                    Name = "JWT Authentication",
+                    Description = "Enter JWT Bearer token **_only_**",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer", // must be lower case
+                    BearerFormat = "JWT",
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+                c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {securityScheme, Array.Empty<string>()}
+            });
+
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "APIs", Version = "v1" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "APIs");
+                //c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
