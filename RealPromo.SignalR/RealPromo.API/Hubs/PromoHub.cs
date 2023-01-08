2d7d9c1a3e846d0fcf9bfcceb38f4acb3b863f61
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-01-08 14:38:15 -0300

    security to SignalR Authorize

--- RealPromo.SignalR/RealPromo.API/Hubs/PromoHub.cs
+++ RealPromo.SignalR/RealPromo.API/Hubs/PromoHub.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 using RealPromo.API.Models;
 using System.Threading.Tasks;
 
 namespace RealPromo.API.Hubs
 {
+
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class PromoHub : Hub
     {
         public async Task CadastrarPromocao(Promocao promocao)
