aae1ac72e1c3fdabeed205b0fca83d80719bcae4
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-01-08 19:34:08 -0300

    Notificador

---
+++ RealPromo.SignalR/RealPromo.API/Notifications/Notificador.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RealPromo.API.Notifications
+{
+    public class Notificador
+    {
+        private List<Notificacao> _notificacoes;
+
+        public Notificador()
+        {
+            _notificacoes = new List<Notificacao>();
+        }
+        public void Handle(Notificacao notificacao)
+        {
+            _notificacoes.Add(notificacao);
+        }
+
+        public List<Notificacao> ObterNotificacoes()
+        {
+            return _notificacoes;
+        }
+
+        public bool TemNotificacao()
+        {
+            return _notificacoes.Any();
+        }
+    }
+}
