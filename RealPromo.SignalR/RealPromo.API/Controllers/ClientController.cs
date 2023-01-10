3c99ad68ebe16a56acfc05ca8d8006c1aa92a3bd
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-01-10 00:28:54 -0300

    endpoint clients

--- RealPromo.SignalR/RealPromo.API/Controllers/ClientController.cs
+++ RealPromo.SignalR/RealPromo.API/Controllers/ClientController.cs
@@ -1,10 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RealPromo.API.Notifications;
+using RealPromo.API.Repository;
+using RealPromo.API.ViewModels;
+using System.Threading.Tasks;
 
 namespace RealPromo.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ClientController
+    public class ClientController : MainController
     {
+        private readonly IClientRepository _clientRepository;
+        public ClientController(INotificador notificador, IClientRepository clientRepository) : base(notificador)
+        {
+            _clientRepository = clientRepository;
+        }
+
+        [HttpGet("clients")]
+        public async Task<ActionResult> Cliente()
+        {
+            return CustomResponse(await _clientRepository.Clients());
+
+        }
     }
 }
