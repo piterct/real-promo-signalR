903110c8e73ddf476b40746be446d9065c0151a1
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-01-10 00:24:10 -0300

    client repository

---
+++ RealPromo.SignalR/RealPromo.API/Repository/IClientRepository.cs
@@ -0,0 +1,11 @@
+﻿using RealPromo.API.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RealPromo.API.Repository
+{
+    public interface IClientRepository
+    {
+        Task<List<ClientsViewModel>> Clients();
+    }
+}
