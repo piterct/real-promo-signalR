00f751f530c14a470c01cc6cf5bdaccdcdba661b
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-01-10 00:21:30 -0300

    ClientRepository

---
+++ RealPromo.SignalR/RealPromo.API/Repository/ClientRepository.cs
@@ -0,0 +1,17 @@
+﻿using RealPromo.API.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RealPromo.API.Repository
+{
+    public class ClientRepository
+    {
+        public async Task<List<ClientsViewModel>> Clients()
+        {
+            return await Task.FromResult(
+                new List<ClientsViewModel>() { new ClientsViewModel { Id = Guid.NewGuid(), Name = "Jhon", MiddleName = "Appex" },
+                { new ClientsViewModel { Id = Guid.NewGuid(), Name = "July", MiddleName = "Manx" } } });
+        }
+    }
+}
