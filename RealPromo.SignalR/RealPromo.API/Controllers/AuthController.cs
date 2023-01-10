25699bc9909cc7c63cd748a316989f038d7367c8
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-01-10 00:06:24 -0300

    autentica endpoint

--- RealPromo.SignalR/RealPromo.API/Controllers/AuthController.cs
+++ RealPromo.SignalR/RealPromo.API/Controllers/AuthController.cs
@@ -1,14 +1,36 @@
-﻿using RealPromo.API.Notifications;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RealPromo.API.Notifications;
 using RealPromo.API.Service;
+using RealPromo.API.ViewModels;
+using System.Threading.Tasks;
 
 namespace RealPromo.API.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class AuthController : MainController
     {
         private readonly IAuthService _authService;
         public AuthController(INotificador notificador, IAuthService authService) : base(notificador)
         {
             _authService = authService;
+        }
+
+        [HttpPost("autentica")]
+        public async Task<ActionResult> Login(LoginUserViewModel loginUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var result = await _authService.GetUser(loginUser.Email, loginUser.Password);
+
+            if (result != null)
+            {
+                return CustomResponse(await _authService.GerarJwt(loginUser.Email, loginUser.Password));
+            }
+
+            NotificarErro("username or password is not valid.");
+            return CustomResponse(loginUser);
         }
     }
 }
