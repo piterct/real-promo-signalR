b8ba701888e97791440803fda2760e1929bfaebb
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-01-11 00:03:09 -0300

    ClientsViewModel

---
+++ RealPromo.SignalR/RealPromo.API/ViewModels/ClientsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RealPromo.API.ViewModels
+{
+    public class ClientsViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string MiddleName { get; set; }
+    }
+}
