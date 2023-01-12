45f7ad137f120eefa700e6eab73c57f149710e78
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-01-12 00:51:50 -0300

    Jwt Config

--- RealPromo.SignalR/RealPromo.API/Configuration/JwtConfig.cs
+++ RealPromo.SignalR/RealPromo.API/Configuration/JwtConfig.cs
@@ -1,6 +1,45 @@
-﻿namespace RealPromo.API.Configuration
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using RealPromo.API.Settings;
+using System.Text;
+
+namespace RealPromo.API.Configuration
 {
-    public class JwtConfig
+    public static class JwtConfig
     {
+
+        public static IServiceCollection AddJwtConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            // JWT
+            var appSettingsSection = configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = true;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.ValidoEm,
+                    ValidIssuer = appSettings.Emissor
+                };
+            });
+
+            return services;
+        }
+
     }
 }
