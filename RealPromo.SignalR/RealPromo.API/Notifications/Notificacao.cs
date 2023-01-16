f72fde37d7cf9c322a23d8a79217ec84f4d44566
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-01-16 00:40:01 -0300

    Notificacao class

---
+++ RealPromo.SignalR/RealPromo.API/Notifications/Notificacao.cs
@@ -0,0 +1,11 @@
+﻿namespace RealPromo.API.Notifications
+{
+    public class Notificacao
+    {
+        public Notificacao(string mensagem)
+        {
+            Mensagem = mensagem;
+        }
+        public string Mensagem { get; }
+    }
+}
