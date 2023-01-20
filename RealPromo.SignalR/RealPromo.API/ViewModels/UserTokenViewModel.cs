c16a3f05467731ca3bbbfb08b62d4ff0db517638
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-01-19 23:01:01 -0300

    User Token View Model

---
+++ RealPromo.SignalR/RealPromo.API/ViewModels/UserTokenViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace RealPromo.API.ViewModels
+{
+    public class UserTokenViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<ClaimViewModel> Claims { get; set; }
+    }
+}
